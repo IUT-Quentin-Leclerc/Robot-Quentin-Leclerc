39fe31e315c9791fc2b5801afa0b586541e875cb
E 105 <vgies@hotmail.com>
2021-01-26 17:17:23 +0100

    commit sceance1

---
+++ 20210126_Robot_leclerc/carte_moteur_leclerc.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=carte_moteur_leclerc.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/carte_moteur_leclerc.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cartemoteurleclerc.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cartemoteurleclerc.x.tar
